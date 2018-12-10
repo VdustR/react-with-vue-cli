a10b785c3dee1014df3c8c0cba19ed3a93df92e6
VdustR <VdustR@gmail.com>
2018-12-10 22:12:30 +0800

    fix base url

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
 module.exports = {
+  baseUrl: process.env.BASE_URL || '/',
   configureWebpack: {
     entry: './src/index',
   },
-
-
   chainWebpack: (config) => {
     config.module
       .rule('babel')
